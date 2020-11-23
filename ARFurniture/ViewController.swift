905fef91a775fe460f60eceef1224a7f6c3ef329
Juan Carlos Estevez <jc_estevezr@hotmail.com>
2020-11-23 16:36:43 -0600

    Initial Commit

---
+++ ARFurniture/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ARFurniture
+//
+//  Created by Juan Carlos on 23/11/20.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
